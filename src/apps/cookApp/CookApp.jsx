22fe04a519907edbf39b9701ba7e2a2cb68a9e77
amine-elgaini <amineelgaini1443@gmail.com>
2023-11-26 12:22:50 +0100

    fix

--- src/apps/cookApp/CookApp.jsx
+++ src/apps/cookApp/CookApp.jsx
@@ -5,19 +5,25 @@ import ProductDetails from "./pages/ProductDetails";
 import Products from "./pages/Products";
 import { Navigate, Route, Routes } from "react-router-dom";
 
-
 export default function CookApp() {
-  return (
-    <>
-      <Navbar/>
-      <Routes>
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="/ApiFinit/" element={<ApiFinit/>}></Route>
-        <Route path="/Products" element={<Products/>}></Route>
-        <Route path="/ProductDetails/:id" element={<ProductDetails/>}></Route>
+    return (
+        <>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="*" element={<Home />}></Route>
+                <Route path="/ApiFinit/" element={<ApiFinit />}></Route>
+                <Route path="/Products" element={<Products />}></Route>
+                <Route
+                    path="/ProductDetails/:id"
+                    element={<ProductDetails />}
+                ></Route>
 
-        <Route path="/redirect" element={<Navigate to="/ApiFinit"/>}></Route>
-      </Routes>
-    </>
-  )
+                <Route
+                    path="/redirect"
+                    element={<Navigate to="/ApiFinit" />}
+                ></Route>
+            </Routes>
+        </>
+    );
 }
